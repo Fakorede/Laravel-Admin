4ffb9a83aa6937538e5669999959d01739f71fe1
moshoodfakorede <moshood@armpension.com>
2020-11-21 19:51:38 +0100

    user update profile

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,8 @@ Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 
 Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('user', 'UserController@user');
+    Route::put('users/info', 'UserController@updateInfo');
+    Route::put('users/password', 'UserController@updatePassword');
     Route::apiResource('users', 'UserController');
 });
