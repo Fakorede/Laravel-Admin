20652e1cafb883253ac35bc300de9137983668db
moshoodfakorede <moshood@armpension.com>
2020-11-22 15:21:29 +0100

    Swagger Documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,27 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Laravel Admin App Documentation",
+     *      description="Laravel Admin App OpenApi description",
+     *      @OA\Contact(
+     *          email="abiolafakorede@gmail.com"
+     *      ),
+     * )
+     *
+     *  @OA\Server(
+     *      url="http://localhost:8000/api",
+     *      description="Laravel Admin API Server"
+     *  )   description="L5 Swagger OpenApi Server"
+     *
+     * @OA\SecurityScheme(
+     *     type="http",
+     *     scheme="bearer",
+     *     securityScheme="bearerAuth",
+     * )
+     */
+
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,6 +15,28 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
+    /**
+     * @OA\Get(
+     *      path="/users",
+     *      operationId="getUsersList",
+     *      tags={"Users"},
+     *      summary="Get list of users",
+     *      description="Returns collection of users",
+     *      @OA\Parameter(
+     *          name="page",
+     *          description="Pagination Page",
+     *          in="query",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(response=200,description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      security={{"bearerAuth": {}}}
+     * )
+     *
+     * Returns list of users
+     */
     public function index()
     {
         Gate::authorize('view', 'users');
@@ -24,6 +46,29 @@ class UserController extends Controller
         return UserResource::collection($users);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/users/{id}",
+     *      operationId="getSingleUser",
+     *      tags={"Users"},
+     *      summary="Get user by id",
+     *      description="Returns a single user",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="User ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(response=200, description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      security={{"bearerAuth": {}}}
+     * )
+     *
+     * Returns a single user by id
+     */
     public function show($id)
     {
         Gate::authorize('view', 'users');
@@ -32,6 +77,24 @@ class UserController extends Controller
         return new UserResource($user);
     }
 
+    /**
+     * @OA\Post(
+     *      path="/users",
+     *      operationId="createUser",
+     *      tags={"Users"},
+     *      summary="Create a User",
+     *      description="Creates a User",
+     *      @OA\Response(response=201, description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/UserCreateRequest")
+     *      ),
+     *      security={{"bearerAuth": {}}},
+     * )
+     *
+     * Creates a User
+     */
     public function store(UserCreateRequest $request)
     {
         Gate::authorize('edit', 'users');
@@ -43,6 +106,33 @@ class UserController extends Controller
         return response(new UserResource($user), Response::HTTP_CREATED);
     }
 
+    /**
+     * @OA\Put(
+     *      path="/users/{id}",
+     *      operationId="updateUser",
+     *      tags={"Users"},
+     *      summary="Update a User",
+     *      description="Updates a User",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="User ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(response=202, description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/UserUpdateRequest")
+     *      ),
+     *      security={{"bearerAuth": {}}},
+     * )
+     *
+     * Updates a User
+     */
     public function update(UserUpdateRequest $request, $id)
     {
         Gate::authorize('edit', 'users');
@@ -53,6 +143,29 @@ class UserController extends Controller
         return response(new UserResource($user), Response::HTTP_ACCEPTED);
     }
 
+    /**
+     * @OA\Delete(
+     *      path="/users/{id}",
+     *      operationId="deleteUser",
+     *      tags={"Users"},
+     *      summary="Delete a User",
+     *      description="Delete a User by Id",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="User ID",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(response=204, description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      security={{"bearerAuth": {}}}
+     * )
+     *
+     * Deletes a user by id
+     */
     public function destroy($id)
     {
         Gate::authorize('edit', 'users');
@@ -61,6 +174,20 @@ class UserController extends Controller
         return response(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/user",
+     *      operationId="getUserDetails",
+     *      tags={"Profile"},
+     *      summary="Get User Details",
+     *      description="Returns details of authenticated user",
+     *      @OA\Response(response=200,description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      security={{"bearerAuth": {}}}
+     * )
+     *
+     * Returns users information
+     */
     public function user()
     {
         $user = Auth::user();
@@ -72,6 +199,24 @@ class UserController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Put(
+     *      path="/users/info",
+     *      operationId="updateUserInfo",
+     *      tags={"Profile"},
+     *      summary="Update User Details",
+     *      description="Updates Authenticated User Details",
+     *      @OA\Response(response=202, description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/UpdateInfoRequest")
+     *      ),
+     *      security={{"bearerAuth": {}}},
+     * )
+     *
+     * Updates a User Information
+     */
     public function updateInfo(UpdateInfoRequest $request)
     {
         $user = Auth::user();
@@ -81,6 +226,24 @@ class UserController extends Controller
         return response(new UserResource($user), Response::HTTP_ACCEPTED);
     }
 
+    /**
+     * @OA\Put(
+     *      path="/users/password",
+     *      operationId="updateUserPassword",
+     *      tags={"Profile"},
+     *      summary="Update User Password",
+     *      description="Updates Authenticated User Password",
+     *      @OA\Response(response=202, description="Successful"),
+     *      @OA\Response(response=400, description="Bad Request"),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/UpdatePasswordRequest")
+     *      ),
+     *      security={{"bearerAuth": {}}},
+     * )
+     *
+     * Updates a User Password
+     */
     public function updatePassword(UpdatePasswordRequest $request)
     {
         $user = Auth::user();
