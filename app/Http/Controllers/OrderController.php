6dede3d6b3c5d2eb0a3e528122062ff15661568f
moshoodfakorede <moshood@armpension.com>
2020-11-22 01:11:42 +0100

    Orders

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\OrderResource;
+use App\Order;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $order = Order::paginate();
+        return OrderResource::collection($order);
+    }
+
+    public function show($id)
+    {
+        return new OrderResource(Order::find($id));
+    }
+}
---
+++ app/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = ['id'];
+
+    public function orderItems()
+    {
+        return $this->hasMany('App\OrderItem');
+    }
+
+    public function getTotalAttribute()
+    {
+        return $this->orderItems->sum(function (OrderItem $item) {
+            return $item->price * $item->quantity;
+        });
+    }
+}
