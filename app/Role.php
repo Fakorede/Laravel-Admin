251770705231fc5ce18004de7b371f469873e79b
moshoodfakorede <moshood@armpension.com>
2020-11-22 02:37:27 +0100

    Creating Permissions

--- app/Role.php
+++ app/Role.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    /**
-     * The attributes that aren't mass assignable.
-     *
-     * @var array
-     */
     protected $guarded = ['id'];
 
     public $timestamps = false;
+
+    public function permissions()
+    {
+        return $this->belongsToMany('App\Permission', 'role_permission');
+    }
 }
