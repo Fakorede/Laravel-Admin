3758b0774eb0167093f383a82cb5172cdb52b954
moshoodfakorede <moshood@armpension.com>
2021-01-11 11:34:44 +0100

    checkout links

--- routes/api.php
+++ routes/api.php
@@ -5,21 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 
-// Shared Routes
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::get('user', 'AuthController@user');
-    Route::put('users/info', 'AuthController@updateInfo');
-    Route::put('users/password', 'AuthController@updatePassword');
-    Route::post('logout', 'AuthController@logout');
-});
-
-// Admin Routes
+// Admin
 Route::group([
     'middleware' => ['auth:api', 'scope:admin'],
     'prefix' => 'admin',
     'namespace' => 'Admin',
 ], function () {
+    Route::post('logout', 'AuthController@logout');
+
     Route::get('chart', 'DashboardController@chart');
+    Route::get('user', 'UserController@user');
+    Route::put('users/info', 'UserController@updateInfo');
+    Route::put('users/password', 'UserController@updatePassword');
     Route::post('upload', 'ImageController@upload');
     Route::get('export', 'OrderController@export');
 
@@ -30,7 +27,7 @@ Route::group([
     Route::apiResource('orders', 'OrderController')->only(['index', 'show']);
 });
 
-// Influencer Routes
+// Influencer
 Route::group([
     'prefix' => 'influencer',
     'namespace' => 'Influencer',
@@ -39,7 +36,15 @@ Route::group([
 
     Route::group([
         'middleware' => ['auth:api', 'scope:influencer'],
-    ], function() {
-        
+    ], function () {
+        Route::post('links', 'LinkController@store');
     });
+});
+
+// Checkout
+Route::group([
+    'prefix' => 'checkout',
+    'namespace' => 'Checkout',
+], function () {
+    Route::get('links/{code}', 'LinkController@show');
 });
